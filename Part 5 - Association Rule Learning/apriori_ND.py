f9d3ef535443d4ede799ebf943849d2a12b021b0
doro1994 <108692154+doro1994@users.noreply.github.com>
2022-07-09 15:55:38 +0300

    Add files via upload

---
+++ Part 5 - Association Rule Learning/apriori_ND.py
@@ -0,0 +1,10 @@
+#Apriori 
+
+# Importing the libraries
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+library(arules)
+# Data preprocessing
+dataset = pd.read_csv('Market_Basket_Optimisation.csv', header = None)
