920e03ef03c4cf48138f13b47493debff0f67785
doro1994 <108692154+doro1994@users.noreply.github.com>
2022-07-09 15:53:01 +0300

    Add files via upload

---
+++ Part 2 - Regression/data_preprocessing_template_ND.py
@@ -0,0 +1,39 @@
+# Data Preprocessing Template
+
+# Importing the libraries
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+# Importing the dataset
+dataset = pd.read_csv('50_Startups.csv')
+X = dataset.iloc[:, :-1].values
+y = dataset.iloc[:, -1].values
+
+# Feature Scaling
+"""from sklearn.preprocessing import StandardScaler
+sc_X = StandardScaler()
+X_train = sc_X.fit_transform(X_train)
+X_test = sc_X.transform(X_test)
+sc_y = StandardScaler()
+y_train = sc_y.fit_transform(y_train)"""
+
+# Encoding the Independent Variable
+from sklearn.preprocessing import LabelEncoder, OneHotEncoder, StandardScaler
+from sklearn.compose import ColumnTransformer, make_column_transformer
+
+preprocess = make_column_transformer(
+        (StandardScaler(), [0]),
+        (StandardScaler(), [1]),
+        (StandardScaler(), [2]),
+        (OneHotEncoder(), [3]),
+        remainder="passthrough")
+
+X = preprocess.fit_transform(X)
+
+# Avoiding the Dummy Variable Trap
+X = X[:, :-1]
+
+# Splitting the dataset into the Training set and Test set
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)
